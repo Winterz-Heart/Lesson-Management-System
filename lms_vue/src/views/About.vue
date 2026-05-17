7177144e7ea8b8a5bd2ef9d716a6951a429fbf23
Winterz-Heart <duncan47685@gmail.com>
2026-05-17 01:44:53 +0100

    Basic Home and About page setup

--- lms_vue/src/views/About.vue
+++ lms_vue/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="hero is-info">
+      <div class="hero-body has-text-centered">
+        <h1 class="title">About</h1>
+      </div>
+    </div>
+    
+    <section class="section">
+      <div  class="has-text-centered mt-10 mb-10">
+        <h1>This my attempt at making a Lesson Management System, using a Django based backend to store all of its data</h1>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    document.title = 'About | LMS'
+  }
+}
+</script>
--- lms_vue/src/views/Home.vue
+++ lms_vue/src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="home">
-    
+    <div class="hero is-info">
+      <div class="her-body has-text-centered">
+        <h1 class="title">Welcome to LMS</h1>
+        <br/>
+        <h2 class="subtitle">An online place for learning course and lessons</h2>
+        <br/>
+      </div>
+    </div>
+
+    <section class="section">
+      <div class="container">
+        <div class="column is-12 has-text-centered">
+          <router-link to="/sign-up" class="button is-info is-size-3 mt-6 mb-6"
+            >Click to get started!</router-link
+          >
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
+  mounted() {
+    document.title = 'Home | LMS'
+  }
 }
 </script>

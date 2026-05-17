0d0b3b740b5b4916b0b1c51cae4562214c8f1ff1
Winterz-Heart <duncan47685@gmail.com>
2026-05-17 04:46:11 +0100

    Added placeholder SourseCards to Homepage;

--- lms_vue/src/views/Home.vue
+++ lms_vue/src/views/Home.vue
@@ -10,6 +10,13 @@
 
     <section class="section">
       <div class="container">
+        <h3 class="subtitle has-text-centered mb-8">Here are some of our Courses!</h3>
+        <div class="columns is-multiline">
+            <div v-for="cardNumber in 4" :key="cardNumber" class="column is-3">
+                <CourseCard />
+            </div>
+        </div>
+
         <div class="column is-12 has-text-centered">
           <router-link to="/sign-up" class="button is-info is-size-3 mt-6 mb-6"
             >Click to get started!</router-link
@@ -21,9 +28,14 @@
 </template>
 
 <script>
+import CourseCard from '../components/CourseCard.vue';
+
 export default {
   mounted() {
     document.title = 'Home | LMS'
+  },
+  components: {
+    CourseCard
   }
 }
 </script>

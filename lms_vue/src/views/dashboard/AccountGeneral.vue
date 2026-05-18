4bb698af1ab685d45660ceb142dec3ad757c08ac
Winterz-Heart <duncan47685@gmail.com>
2026-05-18 21:20:45 +0100

    Refactor AccountGeneral.vue to display user details and improve logout method

--- lms_vue/src/views/dashboard/AccountGeneral.vue
+++ lms_vue/src/views/dashboard/AccountGeneral.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
         <h2 class="title is-4">My Account</h2>
-        <p>Profile details, email, joined date, etc.</p>
-        <br/>
+
+        <template v-if="user">
+            <p><strong>Username:</strong> {{ user.username }}</p>
+            <p><strong>First name:</strong> {{ user.first_name }}</p>
+            <p><strong>Last name:</strong> {{ user.last_name }}</p>
+        </template>
+
+        <template v-else>
+            <p>Loading account details...</p>
+        </template>
+
+        <br />
+        <br />
         <button @click="logout()" class="button is-danger">Log Out</button>
     </div>
 </template>
@@ -11,21 +22,34 @@
 import axios from 'axios';
 
 export default {
+    data() {
+        return {
+            user: null,
+        }
+    },
+    async mounted() {
+        await axios
+            .get('api/v1/users/me')
+            .then(response => {
+                this.user = response.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    },
     methods: {
         async logout() {
-            try {
-                await axios.post("/token/logout/");
-            } catch (error) {
-                console.log(error);
-            } finally {
-                axios.defaults.headers.common["Authorization"] = "";
-
-                localStorage.removeItem("token");
-
-                this.$store.commit("removeToken");
-
-                this.$router.push("/");
-            }
+            await axios
+                .post('/token/logout')
+                .then(() => {
+                    axios.defaults.headers.common['Authorization'] = ''
+                    localStorage.removeItem('token')
+                    this.$store.commit('removeToken')
+                    this.$router.push('/')
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
         },
     },
 }

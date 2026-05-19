0d8876f2e4fdd633d9f15a1f2fa1255539f93fb2
Winterz-Heart <duncan47685@gmail.com>
2026-05-19 12:51:26 +0100

    Register UserProfile model in admin and add admin interface for role management

--- lms_django/courses/admin.py
+++ lms_django/courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Course, CourseProgress
+from .models import Category, Course, CourseProgress, UserProfile
 
 admin.site.register(Category)
 admin.site.register(Course)
@@ -10,3 +10,8 @@ class CourseProgressAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'course', 'status', 'started_at', 'completed_at')
     list_filter = ('status', 'started_at', 'completed_at')
     search_fields = ('user__username', 'user__email', 'course__title')
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+    list_filter = ('role',)

6655a6ea6d92ec17c7432c7e2425d03a5aa282da
Winterz-Heart <duncan47685@gmail.com>
2026-05-20 15:15:29 +0100

    Add AdminDraftCourses component to display all drafted courses

---
+++ lms_vue/src/views/dashboard/AdminDraftCourses.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2 class="title is-4">All Drafted Courses</h2>
+        <br />
+        <template v-if="courses.length === 0" >
+            <p>No courses have been drafted yet</p>
+        </template>
+        <template v-else >
+            <CourseCardViewer :courses="courses" />
+        </template>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import CourseCardViewer from '../../components/CourseCardViewer.vue';
+
+export default {
+    components: {
+        CourseCardViewer
+    },
+    async mounted() {
+        await axios
+            .get('/api/v1/courses/admin/drafts/')
+            .then(response => {
+                this.courses = response.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    },
+    data() {
+        return {
+            courses: []
+        }
+    }
+}
+</script>
